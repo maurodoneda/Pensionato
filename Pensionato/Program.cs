0c3653ed9071603b981d4bc53b508ec18eed3d45
Mauro Doneda <mauro.doneda@gmail.com>
2019-10-29 19:03:30 +0000

    Add project files.

---
+++ Pensionato/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] vet = new Estudantes[10];
+
+            Console.Write("Quantos quartos serao alugados: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtd; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Estudantes(nome, email);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Quartos Ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i+": "+ vet[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ Pensionato/Quartos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensionato
+{
+    class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudantes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+
+    }
+}
